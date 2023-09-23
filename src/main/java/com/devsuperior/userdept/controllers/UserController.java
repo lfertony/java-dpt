90df2163940ba155d28902a3ea51b801d6da2476
Lucas Ferreira <lucas.fertony@gmail.com>
2023-09-23 14:01:20 -0300

    metodo delete implementado

--- src/main/java/com/devsuperior/userdept/controllers/UserController.java
+++ src/main/java/com/devsuperior/userdept/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.devsuperior.userdept.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +38,10 @@ public class UserController {
     public User insert(@RequestBody User user){
         User result = repository.save(user);
         return result;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        repository.deleteById(id);
     }
 }
